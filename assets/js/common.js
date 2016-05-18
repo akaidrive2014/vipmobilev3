e705d0f952086fb844b81976371f4d47ba9942c1
Syarif <syarif@vipplaza.com>
2016-05-18 18:45:32 +0700

    product list and filter

--- assets/js/common.js
+++ assets/js/common.js
@@ -14,4 +14,62 @@ $(function(){
         $(".login").find(".title").html("SILAHKAN LOGIN");
         return false;
     });
+
+    $(".sortnfilter").click(function(){
+        var boxId = $(this).attr('def');
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+        }else{
+            $(".sortnfilter").removeClass('active');
+            $(this).addClass('active');
+        }
+        if($("#"+boxId).is(":visible")){
+            $("#"+boxId).hide();
+        }else{
+            $(".tool-box").hide();
+            $("#"+boxId).show();
+        }
+    });
+
+    $(".filter-point h1").click(function () {
+        $('.filter-point h1').parent().find('ul').slideUp();
+        $('.filter-point h1').find("i").addClass('fa-plus-circle');
+        $('.filter-point h1').find("i").removeClass('fa-minus-circle');
+        if ($(this).parent().find('ul').is(":visible")) {
+            $(this).parent().find('ul').slideUp();
+            $(this).find("i").addClass('fa-plus-circle');
+            $(this).find("i").removeClass('fa-minus-circle');
+        } else {
+            $(this).parent().find('ul').slideDown();
+            $(this).find("i").removeClass('fa-plus-circle');
+            $(this).find("i").addClass('fa-minus-circle');
+        }
+    });
+
+    /*$(".sort").click(function(){
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+        }else{
+            $(".sort").addClass('active');
+        }
+        $("#sort-box").toggle();
+        return false;
+    });
+
+    $(".filter").click(function(){
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+        }else{
+            $(".filter").addClass('active');
+        }
+        $("#filter-box").toggle();
+        return false;
+    });*/
+
+    $("#sort-box li a").click(function(){
+        $("#sort-box li a").removeClass("active");
+        $(this).addClass('active');
+        return false;
+    });
+
 });
